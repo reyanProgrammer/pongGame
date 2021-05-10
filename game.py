ef88031d809602f5d22c2cae8aefda3d3d868329
reyan <reyanmehmood8@gmail.com>
2021-05-10 16:07:46 +0500

    addded basic code

--- game.py
+++ game.py
@@ -1,1 +1,28 @@
-import pygame
+import pygame, sys
+
+# general setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Setting up the main window
+ScreenWidth = 800
+ScreenHeight = 400
+screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
+pygame.display.set_caption('Pong')
+
+# shapes
+ball = pygame.Rect(ScreenWidth / 2 - 15, ScreenWidth / 2 - 15, 30, 30)
+player = pygame.Rect(ScreenWidth - 20, ScreenHeight / 2 - 70, 10, 140)
+opponent = pygame.Rect(10, ScreenHeight/2 - 70, 10, 140)
+
+while True:
+
+    # handling input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        # updating the window
+
+    pygame.display.flip()
+    clock.tick(60)
