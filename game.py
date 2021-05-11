c27856816f2659c4d85ff507921d9032f3709c83
reyan <reyanmehmood8@gmail.com>
2021-05-11 15:41:36 +0500

    added a function

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame, sys
+
 """© reyan mehmood All right reserved"""
 # general setup
 pygame.init()
@@ -24,6 +25,17 @@ ball_x_speed = 7
 ball_y_speed = 7
 
 
+def ball_move():
+    global ball_x_speed, ball_y_speed
+    if ball.top <= 0 or ball.bottom >= ScreenHeight:
+        ball_y_speed *= -1
+    if ball.left <= 0 or ball.right >= ScreenWidth:
+        ball_x_speed *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_x_speed *= -1
+
+
 while True:
 
     # handling input
@@ -36,18 +48,11 @@ while True:
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
-    pygame.draw.aaline(screen, light_grey, (ScreenWidth/2, 0) , (ScreenWidth/2, ScreenHeight))
+    pygame.draw.aaline(screen, light_grey, (ScreenWidth / 2, 0), (ScreenWidth / 2, ScreenHeight))
 
     ball.x += ball_x_speed
     ball.y += ball_y_speed
-
-    if ball.top <= 0 or ball.bottom >= ScreenHeight:
-        ball_y_speed *= -1
-    if ball.left <= 0 or ball.right >= ScreenWidth:
-        ball_x_speed *= -1
-
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_x_speed *= -1
+    ball_move()
 
     # updating the window
     pygame.display.flip()
